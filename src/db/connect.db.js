441e43488ec12d95b1a203ad8720926ad47ade9e
nikhil-dev-395 <nikhilw395@gmail.com>
2025-06-24 17:05:54 +0530

    feat :  experience is added in hardcoded

--- src/db/connect.db.js
+++ src/db/connect.db.js
@@ -1,15 +1,14 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
 
-const connectDB = async()=>{
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URL);
-        console.log("mongodb connected .."+ conn.connection.host);
-        return conn;
-
-    } catch (error) {
-        console.log("db connection error ", error);
-
-    }
-}
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URL);
+    console.log("mongodb connected .." + conn.connection.host);
+    return conn;
+  } catch (error) {
+    console.log("db connection error ", error);
+    process.exit(1);
+  }
+};
 
 module.exports = connectDB;
